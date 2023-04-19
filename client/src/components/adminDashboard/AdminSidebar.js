5e5841348213a92f5d9facc8414f2fbcbb757d0f
Alex Castaneda <alex@Alejandros-Mac.local>
2023-04-18 20:31:17 -0700

    gave def. of admin sidebar component

---
+++ client/src/components/adminDashboard/AdminSidebar.js
@@ -0,0 +1,20 @@
+
+  
+  
+  
+  
+  
+  
+  
+  // defines a AdminSidebar component with a logout function that logs the user out, shows a toast notification to show successful logout, and redirects user to the home page 
+  const AdminSidebar = (props) => {
+    const navigate = useNAvigate();
+    const logout = async () => {
+        const res = await fetch('/logout')
+        props.setToastCondition({
+            status: 'success',
+            message: 'Log Out Successful!',
+        });
+        props.setToastShow(true);
+        navigate('/');
+}};
