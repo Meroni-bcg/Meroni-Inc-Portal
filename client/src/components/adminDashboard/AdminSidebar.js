7d370e396c06aad4bf6e5c8a4df518e0c87ac7b6
Alex Castaneda <alex@Alejandros-Mac.local>
2023-04-18 21:05:08 -0700

    switch branches

--- client/src/components/adminDashboard/AdminSidebar.js
+++ client/src/components/adminDashboard/AdminSidebar.js
@@ -6,7 +6,7 @@
 
 
 
-// defines a AdminSidebar component with a logout function that logs the user out, shows a toast notification to show successful logout, and redirects user to the home page
+
 const AdminSidebar = (props) => {
     const navigate = useNAvigate();
     const logout = async () => {
