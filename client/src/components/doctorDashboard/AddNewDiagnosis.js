af726f4ae94cac3d9f9eba497c1684236b3ae8c6
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-04-18 19:21:56 -0700

    created dependencies and declared state variables for new Prescriptions

--- client/src/components/doctorDashboard/AddNewDiagnosis.js
+++ client/src/components/doctorDashboard/AddNewDiagnosis.js
@@ -1,4 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import ReactLoading from "react-loading"; 
+//Import necessary dependencies from react and react-router-dom
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+//Create a functional component name 
+const AddNewPrescription = (props) => {
+
+//declare state variables
+const [isLoading, setIsLoading] = useState(false);
+const [physician, setPhysician] = useState({});
+const [prescriptionList, setPrescriptionList] = useState ([
+    {
+        prescriptionName: '',
+        dosage: {
+            morning: {quantity: '', comment: ''},
+            afternoon: {quantity: '', comment: ''},
+            evening: {quantity: '', comment: ''},
+        }
+    }
+])
+
+}
