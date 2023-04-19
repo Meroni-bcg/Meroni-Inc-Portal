a9e08f83e3451e3dfaac448524b3286c6a98bd05
Tara Maddox <TMaddox85@gmail.com>
2023-04-19 00:21:03 -0700

    started a fresh app.js/made routes for patients

--- app.js
+++ app.js
@@ -1,50 +1,36 @@
-const express = require("express");
-const dotenv = require("dotenv");
-//const { default: mongoose } = require("mongoose");
-const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+const express = require('express');
 const patientRoutes = require("./routes/patientRoutes");
-const cookieParser = require("cookie-parser");
-const registerRoute = require("./routes/registerRoute");
-const doctorRoute = require("./routes/doctorRoute");
-const adminRoutes = require("./routes/adminRoutes");
-const logoutRoute = require("./routes/logoutRoute");
-const cors = require("cors");
-const app = express();
 const mongoose = require('mongoose')
 
-dotenv.config({ path: "./config.env" });
+//express app
+const app = express();
 
-app.use(cors());
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cookieParser());
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+}),
 
-const dbURI = process.env.DATABASE;
-const port = process.env.PORT || 3000;
+/*route
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome to Meroni-Inc-Portal'})
+}),*/
+
+app.use('/', patientRoutes)
+
+
+
+// listens for request
+app.listen(process.env.PORT, () => {
+  console.log('listening on port 3000')
+})
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
   () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
-/*mongoose
-  .connect(dbURI)
-  .then((result) => {
-    app.listen(port);
-    console.log("connected to db and listening at port 3000");
-  })
-  .catch((err) => {
-    app.listen(port);
-    app.get("/", (req, res) => {
-      res.send(
-        "Something Went Wrong! Please Try again after some time, if problem persists please contact us."
-      );
-    });
-  });*/
 
-app.use(authRoutes);
-app.use(registerRoute);
-app.use(doctorRoute);
-app.use(patientRoutes);
-app.use(adminRoutes);
-app.use(logoutRoute);
 
+process.env
