7873706bf168b57d13ebbb0dbad28fc10500c8c1
Karlie Griffin <113546703+KGriffin12@users.noreply.github.com>
2023-04-20 20:38:48 -0500

    Added if statement for incorrect email.

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -1,3 +1,34 @@
 const Patient = require("../models/patient");
 const { createToken } = require("../utils/createToken");
 
+const maxAge = 3 * 24 * 60 * 60;
+const handleError = (err) => {
+    let errors = {};
+
+//Incorrect email
+if (err.message === "Invalid HealthID") {
+    errors.healthID = "That Health ID is not registered";
+}
+
+//Incorrect password
+
+//duplicate error code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
