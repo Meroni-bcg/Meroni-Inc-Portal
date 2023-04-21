f5d44c0cd2d62c1c61650a27cb9233e64d2b93f1
Alex Castaneda <alex@Alejandros-Mac.local>
2023-04-20 18:57:54 -0700

    updated admin sideBar

--- client/src/components/adminDashboard/AdminSidebar.js
+++ client/src/components/adminDashboard/AdminSidebar.js
@@ -1,12 +1,13 @@
-
+import logo from "";
+import add_doctor from "";
+import patient_list from "";
+import doctor_list from "";
+import logoutimg from "";
+import { Link, Outlet, useNavigate } from "";
+import { useState } from "react";
+import dashboard from "";
   
-  
-  
-  
-  
-  
-  
-  // defines a AdminSidebar component with a logout function that logs the user out, shows a toast notification to show successful logout, and redirects user to the home page 
+// defines a AdminSidebar component with a logout function that logs the user out, shows a toast notification to show successful logout, and redirects user to the home page 
   const AdminSidebar = (props) => {
     const navigate = useNAvigate();
     const logout = async () => {
